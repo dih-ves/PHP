8bb918e58e4177d47793efbeec6ef613c01e8827
Diego Galves <53906295+dih-ves@users.noreply.github.com>
2020-01-17 12:22:40 -0300

    Update predef_var.php

--- predef_var.php
+++ predef_var.php
@@ -1,4 +1,32 @@
 <?php
 echo $_SERVER['SCRIPT_NAME'];
 //Output "/somefile.php"
+
+//exemplo 2:
+
+$addr = $_SERVER['SCRIPT_NAME];
+echo $addr;
+
+///////////////////////////////
+
+echo $_SERVER['HTTP_HOST];
+//Outputs "Local host"
+
+//////////////////////////////
+
+//CREATE A FILE TO MANAGER THE ADDRESS OF ALL IMAGES
+
+$host = $_SERVER['HTTP_HOST'];
+$image_path = $host.'/images/';
+
+//CONFIG PHP FILE IN SCRIPTS
+
+require 'config.php';
+echo '<img src="'.$image_path.'header.png" />';
+
+// $_SERVER['HTTP_HOST'] returns the Host header from the current request. 
+
+
+
+
 ?>
